5d378fc8dc3bee9e9d5fa43e9c3185953962c417
雷永祥 <76198186+arsihhh@users.noreply.github.com>
2024-02-04 09:31:52 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o2w1k6ZltziJbkjAM1TZ87NkB_cA',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'h89hofKJ2lx8iA1oZsczAfpJBOfpr7_X5nrx6nUdlxk',
+      useTemplateId: 'skESTq1n4JXJv2PxWDUwb_6Z8drgjd471uCGamZKuF0',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '04-11',
       festivals: [
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'h89hofKJ2lx8iA1oZsczAfpJBOfpr7_X5nrx6nUdlxk',
+  CALLBACK_TEMPLATE_ID: 'skESTq1n4JXJv2PxWDUwb_6Z8drgjd471uCGamZKuF0',
 
   CALLBACK_USERS: [
     {
