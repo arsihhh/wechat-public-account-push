5d8b03ed605104aef29cc63a49c8345355e30fad
雷永祥 <76198186+arsihhh@users.noreply.github.com>
2024-02-04 09:59:25 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -26,6 +26,7 @@ const USER_CONFIG = {
       useTemplateId: 'skESTq1n4JXJv2PxWDUwb_6Z8drgjd471uCGamZKuF0',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '04-11',
+      horoscopeDateType: '今日',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
